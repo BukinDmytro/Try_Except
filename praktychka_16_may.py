73d6bffc3e07bc1708130b25814c0f8bc6af3400
student <bukindimon444@gmail.com>
2023-05-16 20:45:41 +0300

    Трошки покращена практичка..

--- praktychka_16_may.py
+++ praktychka_16_may.py
@@ -4,11 +4,12 @@ class InvalidUsernameError(Exception):
     def __init__(self,username):
         self.username = username
 
-#InvalidSpaceError
 #NameIsAlreadyTakenError ДОРОБИТИ ПРИДУМАТИ !!!!!
 
 def register_user(username):
     if len(username) <= 5:
+        raise InvalidUsernameError(username)
+    if "!" or "," or ";" or "%" or "#" or " " in username:
         raise InvalidUsernameError(username)
     else:
         print("Ви успішно зареєструвались !")
@@ -19,7 +20,7 @@ try:
     username = input("Введіть ім'я користувача: ")
     register_user(username)
 except InvalidUsernameError:
-    print("Недостатньо символів у Вашому імені користувача ! Спробуйте щось інше ")
+    print("Недостатньо символів у Вашому імені користувача або є недопустимі символи ! Спробуйте ще раз")
 
 
 #2
@@ -28,11 +29,12 @@ class InvalidPasswordLength(Exception):
     def __init__(self,password):
         self.password = password
 
-#InvalidSpaceError
-#NameIsAlreadyTakenError ДОРОБИТИ ПРИДУМАТИ !!!!!
+#PasswordIsAlreadyTakenError ДОРОБИТИ ПРИДУМАТИ !!!!!
 
 def register_password(password):
     if len(password) <= 5:
+        raise InvalidPasswordLength(password)
+    if " " in password:
         raise InvalidPasswordLength(password)
     else:
         print("Пароль надійний !")
@@ -45,7 +47,7 @@ try:
 except InvalidPasswordLength:
     print("Недостатньо символів у Вашому паролі ! Спробуйте щось інше ")
 
-#3
+#3 СПОСІБ НОМЕР ОДИН
 
 class InvalidFileFormatError(Exception):
     def __init__(self , filename):
@@ -63,5 +65,29 @@ try:
 except InvalidFileFormatError:
     print("Недопустимий формат файлу !")
 
+
+#3 ЗАВДАННЯ СПОСІБ НОМЕР ДВА
+
+
+class InvalidFileFormatError(Exception):
+    def __init__(self,f):
+        self.f = f
+
+#FileNotFoundError !!!!
+#UnsupportedFormatError - коли нема розширення ...
+#....
+def read_file(f):
+    try:
+        with open(f , "r") as file:
+            content = file.read()
+            print("Вміст файлу:" , content)
+    except IOError:
+        raise InvalidFileFormatError(f)
+
+try:
+    read_file(input("Введіть назву файлу:"))
+except InvalidFileFormatError as e:
+    print(f"Невірний формат файлу {e.f}"
+          f" Підтримуються тількі текстові файли")
 
 
