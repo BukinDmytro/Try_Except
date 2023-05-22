31df180d2156075e8b9cd8f27d375c07a16da979
dima014 <bukindima222@gmail.com>
2023-05-22 23:10:05 +0300

    Перше завдання

---
+++ dz_26_05_23.py
@@ -0,0 +1,19 @@
+#1
+
+class InvalidUrlError(Exception):
+    def __init__(self,url):
+        self.url = url
+def fetch_data_from_url(url):
+        if url != "https://www.youtube.com/watch?v=Gs_uBw0CJNk":
+            raise InvalidUrlError("Invalid URL:" + url)
+        elif "/" not in url:
+            raise InvalidUrlError("Something was typed uncorrectly (")
+        else:
+            print("URL was typed correctly")
+
+try:
+    fetch_data_from_url("https://www.youtube.com/watch?v=Gs_uBw0CJNK")
+except InvalidUrlError as e:
+    print("Error:" , e)
+
+
